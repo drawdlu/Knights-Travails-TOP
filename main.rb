9c9cd7ab0e34d5377d80bfafe4f615a3f08470d5
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-01-23 20:26:01 +0800

    refactor: Check if valid before adding to array

--- main.rb
+++ main.rb
@@ -10,19 +10,17 @@ def valid_moves(node)
 
   possible_moves.each do |move|
     vertex = [node.vertex[0] + move[0], node.vertex[1] + move[1]]
-    valid_arr.push(Node.new(vertex, node))
+    valid_arr.push(Node.new(vertex, node)) if valid?(vertex)
   end
-
-  valid_arr.delete_if { |move| not_valid?(move.vertex) }
 
   valid_arr
 end
 
-def not_valid?(move)
-  return true unless move[0] >= 0 && move[0] <= 7 && move[1] >= 0 && move[1] <= 7
+def valid?(move)
+  return true if move[0] >= 0 && move[0] <= 7 && move[1] >= 0 && move[1] <= 7
 
   false
 end
 
-test = Node.new([1, 1])
+test = Node.new([3, 3])
 valid_moves(test).each { |node| print "#{node.vertex} \n" }
